7fb0f55e736e00fe744330a04e4a9dc80a15039c
marin <marin.glavas@outlook.com>
2023-03-18 17:03:45 +0100

    refactor navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,34 @@
-import {
-  NavigationContainer,
-  NavigationProp,
-  ParamListBase,
-} from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React, {useState} from 'react';
-import {SafeAreaView, StatusBar, Text, useColorScheme} from 'react-native';
+import React, {createContext, useContext, useState} from 'react';
+import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
 import {Button, TextInput} from 'react-native-paper';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+type UserContext = State<string | undefined>;
+type PairContext = State<string | undefined>;
+type OnBoardingContext = State<boolean>;
+type State<T> = [T, React.Dispatch<React.SetStateAction<T>>];
+
+const OnBoardingContext = createContext<OnBoardingContext>([false, () => {}]);
+const UserContext = createContext<UserContext>(['', () => {}]);
+const PairContext = createContext<PairContext>(['', () => {}]);
+
+enum Routes {
+  OnBoarding = 'OnBoarding',
+  Messages = 'Messages',
+}
+
+enum OnBoardingRoutes {
+  Registration = 'Registration',
+  Pairing = 'Pairing',
+}
+
+enum MessagesRoutes {
+  KissRequest = 'KissRequest',
+  KissSelection = 'KissSelection',
+}
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -18,53 +38,55 @@ function App(): JSX.Element {
   };
 
   const Stack = createNativeStackNavigator();
-
+  const [isOnboarded, setIsOnboarded] = useState(false);
   return (
-    <SafeAreaView style={{...backgroundStyle, flex: 1}}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name={Routes.Registration}
-            component={Registration}
-            options={{headerShown: false}}
+    <NavigationContainer>
+      <OnBoardingContext.Provider value={[isOnboarded, setIsOnboarded]}>
+        <SafeAreaView style={{...backgroundStyle, flex: 1}}>
+          <StatusBar
+            barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+            backgroundColor={backgroundStyle.backgroundColor}
           />
-          <Stack.Screen
-            name={Routes.Messages}
-            component={Messages}
-            options={{headerShown: false}}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaView>
+          <Stack.Navigator>
+            {isOnboarded ? (
+              <Stack.Screen
+                name={Routes.Messages}
+                component={Messages}
+                options={{headerShown: false}}
+              />
+            ) : (
+              <Stack.Screen
+                name={Routes.OnBoarding}
+                component={OnBoarding}
+                options={{headerShown: false}}
+              />
+            )}
+          </Stack.Navigator>
+        </SafeAreaView>
+      </OnBoardingContext.Provider>
+    </NavigationContainer>
   );
 }
 
-enum Routes {
-  Registration = 'Registration',
-  Messages = 'Messages',
-}
-
-enum SetupRoutes {
-  UserSetup = 'UserSetup',
-  Pairing = 'Pairing',
-}
-
-enum MessagesRoutes {
-  KissRequest = 'KissRequest',
-  KissSelection = 'KissSelection',
-}
-
-const Registration = () => {
+const OnBoarding = () => {
   const Stack = createNativeStackNavigator();
+  const [username, setUsername] = useState<string | undefined>('');
   return (
-    <Stack.Navigator>
-      <Stack.Screen name={SetupRoutes.UserSetup} component={UserSetup} />
-      <Stack.Screen name={SetupRoutes.Pairing} component={Pairing} />
-    </Stack.Navigator>
+    <UserContext.Provider value={[username, setUsername]}>
+      <Stack.Navigator>
+        {username ? (
+          <Stack.Screen
+            name={OnBoardingRoutes.Pairing}
+            component={InitialPairing}
+          />
+        ) : (
+          <Stack.Screen
+            name={OnBoardingRoutes.Registration}
+            component={Registration}
+          />
+        )}
+      </Stack.Navigator>
+    </UserContext.Provider>
   );
 };
 const Messages = () => {
@@ -89,18 +111,19 @@ const KissSelection = ({navigation}) => (
     Kiss selection
   </Button>
 );
-const UserSetup = ({navigation}) => {
+const Registration = () => {
+  const [persistedUsername, setPersistedUsername] = useContext(UserContext);
   const [username, setUsername] = useState('');
   const onRegister = async () => {
     console.log(username);
-    navigation.push(SetupRoutes.Pairing);
+    setPersistedUsername(username);
   };
   return (
     <>
       <TextInput
         placeholder="Username..."
         mode="outlined"
-        value={username}
+        value={username || persistedUsername}
         onChangeText={setUsername}
       />
       <Button
@@ -113,18 +136,19 @@ const UserSetup = ({navigation}) => {
     </>
   );
 };
-const Pairing = ({navigation}) => {
+const Pairing = () => {
+  const [persistedPair, setPersistedPair] = useContext(PairContext);
   const [pair, setPair] = useState('');
   const onPaired = () => {
     console.log(pair);
-    navigation.replace(Routes.Messages);
+    setPersistedPair(pair);
   };
   return (
     <>
       <TextInput
         placeholder="Pair..."
         mode="outlined"
-        value={pair}
+        value={pair || persistedPair}
         onChangeText={setPair}
       />
       <Button
@@ -134,7 +158,21 @@ const Pairing = ({navigation}) => {
         onPress={() => onPaired()}>
         Pair
       </Button>
-      <Button mode="outlined" style={{marginTop: 10}}>
+    </>
+  );
+};
+const InitialPairing = () => {
+  const [_, setIsOnboarded] = useContext(OnBoardingContext);
+  const onPairLater = () => {
+    setIsOnboarded(true);
+  };
+  return (
+    <>
+      <Pairing />
+      <Button
+        mode="outlined"
+        style={{marginTop: 10}}
+        onPress={() => onPairLater()}>
         Pair later
       </Button>
     </>
